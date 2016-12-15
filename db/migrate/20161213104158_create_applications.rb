3741a12d6e8af41fcf34a9142cb0f32dc289a00f
Michael Dawson <mdawson@marketplacer.com>
2016-12-15 22:35:04 +1100

    Rework application model

--- db/migrate/20161213104158_create_applications.rb
+++ db/migrate/20161213104158_create_applications.rb
@@ -1,17 +1,15 @@
 class CreateApplications < ActiveRecord::Migration[5.0]
   def change
     create_table :applications do |t|
-      t.text :team
       t.text :problem
       t.text :solution
-      t.text :uvp
-      t.text :segments
-      t.text :metrics
-      t.text :channels
-      t.text :cost_structure
+      t.text :target_market
       t.text :revenue_streams
+      t.text :channels
+      t.text :traction
       t.text :funding
-      t.text :funding_equity
+      t.text :team
+      t.text :other
 
       t.timestamps
     end
