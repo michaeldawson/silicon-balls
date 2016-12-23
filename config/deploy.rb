97cee26727a07070cbd6dd4095a105f0c7a4dd5d
Michael Dawson <mdawson@marketplacer.com>
2016-12-24 09:44:28 +1100

    Update capistrano setup for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.6.1'
+lock '3.7.1'
 
 set :application, 'silicon_balls'
 set :repo_url, 'https://github.com/michaeldawson/silicon-balls.git'
