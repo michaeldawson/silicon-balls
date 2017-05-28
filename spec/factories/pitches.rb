34f09b71f01a7f97f36bb24d9f428fad6316df64
Michael Dawson <mdawson@marketplacer.com>
2017-05-28 12:36:19 +1000

    Add pitch factory

---
+++ spec/factories/pitches.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :pitch do
+    company_name 'Test company'
+    problem 'Test problem'
+    solution 'Test solution'
+    target_market 'Test target market'
+    revenue_streams 'Test revenue streams'
+    channels 'Test channels'
+    traction 'Test traction'
+    funding 'Test funding'
+    team 'Test team'
+    other 'Test other'
+  end
+end
